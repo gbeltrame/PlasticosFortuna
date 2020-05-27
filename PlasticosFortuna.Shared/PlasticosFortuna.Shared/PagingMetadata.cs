774dfcc290fd2902f07f0deb0ca241caad94f2af
gbeltrame <beltramegustavo87@gmail.com>
2020-05-27 02:51:04 -0300

    Paging Custom Headers Read

---
+++ PlasticosFortuna.Shared/PlasticosFortuna.Shared/PagingMetadata.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PlasticosFortuna.Shared
+{
+    public class PagingMetadata
+    {
+		public int CurrentPage { get; set; }
+		public int TotalPages { get; set; }
+		public int PageSize { get; set; }
+		public int TotalCount { get; set; }
+		public bool HasPrevious { get; set; }
+		public bool HasNext { get; set; }
+	}
+}
