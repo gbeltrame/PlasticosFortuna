70be43f06daa815f8b7d794fc91e29ab33e8d0d8
gbeltrame <beltramegustavo87@gmail.com>
2020-06-12 03:37:28 -0300

    Logout Agregado

--- PlasticosFortuna.UI/Data/CustomAuthenticationStateProvider.cs
+++ PlasticosFortuna.UI/Data/CustomAuthenticationStateProvider.cs
@@ -43,10 +43,18 @@ namespace PlasticosFortuna.UI.Data
         public void MarkUserAsAutheticated(string loginId)
         {
             var identity = new ClaimsIdentity(new[]
-{
+            {
                 new Claim(ClaimTypes.Name, loginId),
 
             }, "apiauth_type");
+            var user = new ClaimsPrincipal(identity);
+            NotifyAuthenticationStateChanged(Task.FromResult(new AuthenticationState(user)));
+        }
+
+        public void MarkUserAsLoggedOut()
+        {
+            _sessionStorageService.RemoveItemAsync("loginId");
+            var identity = new ClaimsIdentity();
             var user = new ClaimsPrincipal(identity);
             NotifyAuthenticationStateChanged(Task.FromResult(new AuthenticationState(user)));
         }
